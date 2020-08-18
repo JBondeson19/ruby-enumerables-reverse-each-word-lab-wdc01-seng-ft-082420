c9c2a8dcd9428e3f46060f9858b4cc2a3c093df8
James P Bondeson <jpbond58@gmail.com>
2020-08-18 02:57:27 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 require "pry"
 
- sentence1 = "Hello there, and how are you?"
-sentence2 = "Hi again, just making sure it's reversed!"
+ #sentence1 = "Hello there, and how are you?"
+#sentence2 = "Hi again, just making sure it's reversed!"
 
 def reverse_each_word(sentence1)
 
