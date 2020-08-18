5ecc9c11ebdaad0fb7ce38d2d0ed3908dfc11f05
James P Bondeson <jpbond58@gmail.com>
2020-08-18 03:09:23 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,20 +1,11 @@
 require "pry"
 
-#sentence1 = "Hello there, and how are you?"
+ #sentence1 = "Hello there, and how are you?"
 #sentence2 = "Hi again, just making sure it's reversed!"
-binding.pry
+
 def reverse_each_word(sentence1)
 
-new_output = []
 array = sentence1.split
-  new_output << array.map { |item| item.reverse }
+  new_output = array.collect { |item| item.reverse }
    new_output.join(" ")
-end
-
-
-
-
-
-def reverse_each_word(sentence2)
-
 end
